c973b59812f8099cd55dc983421a77fdba3272ff
Tom Misawa <riversun.org@gmail.com>
2014-02-05 05:00:20 +0900

    Add BigFileSearcher class

---
+++ src/main/java/org/riversun/bigdoc/bin/BigFileSearcher.java
@@ -0,0 +1,468 @@
+/*  bigdoc Java lib for easy to read/search from a big document
+ *
+ *  Copyright (c) 2006-2016 Tom Misawa, riversun.org@gmail.com
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *  
+ */
+package org.riversun.bigdoc.bin;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.riversun.bigdoc.bin.BinFileSearcher.BinFileProgressListener;
+
+/**
+ * 
+ * Search bytes from big file<br>
+ * Available for giga-bytes order file <br>
+ * 
+ * @author Tom Misawa (riversun.org@gmail.com)
+ */
+public class BigFileSearcher {
+
+	public static interface BigFileProgressListener {
+		public void onProgress(float progress);
+	}
+
+	private interface BinFileProgressListenerEx {
+		public void onProgress(int workerNumber, int workerSize, List<Long> pointerList, float progress);
+	}
+
+	private ProgressCache progressCache;
+
+	private boolean useOptimization = true;
+
+	/**
+	 * Use memory and threading optimization<br>
+	 * (default is true)
+	 * 
+	 * @param enabled
+	 */
+	public void setUseOptimization(boolean enabled) {
+		this.useOptimization = enabled;
+	}
+
+	private final BinFileSearcher binFileSearcher = new BinFileSearcher();
+
+	// max number of thread
+	public static final int DEFAULT_MAX_NUM_OF_THREADS = 24;
+
+	// thread number no limit
+	public static final int THREADS_NO_LIMIT = 0;
+
+	// Unit size when split loading
+	public static final int DEFAULT_BLOCK_SIZE = 10 * 1024 * 1024;
+
+	private int bufferSizePerWorker = BinFileSearcher.DEFAULT_BUFFER_SIZE;
+
+	/**
+	 * Number of threads used at the same time in one search
+	 */
+	private int subThreadSize = BinFileSearcher.DEFAULT_SUB_THREAD_SIZE;
+
+	/**
+	 * The size of the window used to scan memory
+	 */
+	private int subBufferSize = BinFileSearcher.DEFAULT_SUB_BUFFER_SIZE;
+
+	/**
+	 * Size per unit when divide loading big sized file into multiple pieces<br>
+	 */
+	private int blockSize = DEFAULT_BLOCK_SIZE;
+
+	private int maxNumOfThreads = DEFAULT_MAX_NUM_OF_THREADS;
+
+	private BigFileProgressListener bigFileProgressListener;
+
+	private long _profile_lastStartTime;
+	private long _profile_lastEndTime;
+
+	/**
+	 * Set progress listener
+	 * 
+	 * @param listener
+	 */
+	public void setBigFileProgressListener(BigFileProgressListener listener) {
+		this.bigFileProgressListener = listener;
+	}
+
+	/**
+	 * Set size per unit when divide loading big sized file into multiple pieces<br>
+	 * <br>
+	 * In order to make this method effective,call setUseOptimization(false) to
+	 * turn off the optimization.<br>
+	 * 
+	 * @param blockSize
+	 */
+	public void setBlockSize(int blockSize) {
+		this.blockSize = blockSize;
+	}
+
+	/**
+	 * Set size to be read into memory at one search <br>
+	 * In order to make this method effective,call setUseOptimization(false) to
+	 * turn off the optimization.<br>
+	 * 
+	 * @param bufferSize
+	 */
+	public void setBufferSizePerWorker(int bufferSize) {
+		this.bufferSizePerWorker = bufferSize;
+	}
+
+	/**
+	 * Set max number of thread to concurrent load to file <br>
+	 * Increased the number of threads does not means improving a performance<br>
+	 * <br>
+	 * In order to make this method effective,call setUseOptimization(false) to
+	 * turn off the optimization.<br>
+	 * 
+	 * @param maxNumOfThreads
+	 */
+	public void setMaxNumOfThreads(int maxNumOfThreads) {
+		this.maxNumOfThreads = maxNumOfThreads;
+	}
+
+	/**
+	 * Set number of threads used in each worker <br>
+	 * In order to make this method effective,call setUseOptimization(false) to
+	 * turn off the optimization.<br>
+	 * 
+	 * @param subThreadSize
+	 */
+	public void setSubThreadSize(int subThreadSize) {
+		this.subThreadSize = subThreadSize;
+	}
+
+	/**
+	 * Set the size of the window used to scan memory used in each worker <br>
+	 * In order to make this method effective,call setUseOptimization(false) to
+	 * turn off the optimization.<br>
+	 * 
+	 * @param subBufferSize
+	 */
+	public void setSubBufferSize(int subBufferSize) {
+		this.subBufferSize = subBufferSize;
+	}
+
+	/**
+	 * Returns the index within this file of the first occurrence of the
+	 * specified substring.
+	 * 
+	 * @param f
+	 * @param searchBytes
+	 * @return
+	 */
+	public Long indexOf(File f, byte[] searchBytes) {
+		return indexOf(f, searchBytes, 0);
+	}
+
+	/**
+	 * Returns the index within this file of the first occurrence of the
+	 * specified substring, starting at the specified position.
+	 * 
+	 * @param f
+	 *            target file
+	 * @param searchBytes
+	 *            a sequence of bytes you want to find
+	 * @param fromPosition
+	 *            "0" means the beginning of the file
+	 * @return position of the first occurence. '-1' means that it was not
+	 *         found.
+	 */
+	public Long indexOf(File f, byte[] searchBytes, long fromPosition) {
+		return binFileSearcher.indexOf(f, searchBytes, fromPosition);
+	}
+
+	/**
+	 * Search bytes faster in a concurrent processing.
+	 * 
+	 * @param srcBytes
+	 * @param searchBytes
+	 * @return
+	 */
+	public List<Long> searchBigFile(File srcFile, byte[] searchBytes) {
+
+		int numOfThreadsOptimized = (int) (srcFile.length() / (long) blockSize);
+
+		if (numOfThreadsOptimized == 0) {
+			numOfThreadsOptimized = 1;
+		}
+
+		return searchBigFile(srcFile, searchBytes, numOfThreadsOptimized);
+	}
+
+	/**
+	 * Search bytes faster in a concurrent processing with concurrency level.
+	 * 
+	 * @param srcBytes
+	 * @param searchBytes
+	 * @param numOfThreads
+	 * @return
+	 */
+	private List<Long> searchBigFile(File srcFile, byte[] searchBytes, int numOfThreads) {
+
+		if (useOptimization) {
+			optimize(srcFile.length());
+		}
+
+		_profile_lastStartTime = System.currentTimeMillis();
+
+		if (numOfThreads == 0) {
+			numOfThreads = 1;
+		}
+
+		final long sizeOfSrcBytes = srcFile.length();
+		final int sizeOfSearchBytes = searchBytes.length;
+		final long bytesToReadBlockSize = (sizeOfSrcBytes - (long) sizeOfSearchBytes) / (long) numOfThreads;
+
+		final int threadPoolSize;
+
+		if (maxNumOfThreads == THREADS_NO_LIMIT) {
+			threadPoolSize = numOfThreads;
+		} else {
+			threadPoolSize = maxNumOfThreads;
+		}
+
+		final ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+
+		final List<Future<List<Long>>> futureList = new ArrayList<Future<List<Long>>>();
+
+		for (int i = 0; i < numOfThreads; i++) {
+
+			final long offset = bytesToReadBlockSize * (long) i;
+			final long readLeng;
+
+			if (i == numOfThreads - 1) {
+				// if it's the last element.
+				readLeng = sizeOfSrcBytes - offset - 1;
+			} else {
+				// else , add the overlapping part size to blockSize
+				readLeng = bytesToReadBlockSize + sizeOfSearchBytes;
+			}
+
+			final BinFileProgressListenerEx progressListener;
+
+			if (bigFileProgressListener == null) {
+				progressListener = null;
+			} else {
+				progressListener = new BinFileProgressListenerEx() {
+
+					@Override
+					public void onProgress(int workerNumber, int workerSize, List<Long> pointerList, float progress) {
+						BigFileSearcher.this.onProgress(workerNumber, workerSize, pointerList, progress);
+					}
+				};
+			}
+
+			final int workerSize = numOfThreads;
+			final int workerNumber = i;
+
+			final Future<List<Long>> future = executorService.submit(new BigFileSearchTask(srcFile, searchBytes, offset, readLeng, workerNumber, workerSize, progressListener));
+
+			futureList.add(future);
+		}
+		executorService.shutdown();
+
+		// Remove duplicate indexes
+		final List<Long> resultIndexList = new CopyOnWriteArrayList<Long>();
+
+		for (Future<List<Long>> future : futureList) {
+
+			try {
+				List<Long> rawIndexList = future.get();
+
+				for (int i = 0; i < rawIndexList.size(); i++) {
+
+					Long longVal = rawIndexList.get(i);
+
+					if (resultIndexList.contains(longVal)) {
+						// if already exists , skip
+					} else {
+						resultIndexList.add(longVal);
+					}
+				}
+			} catch (InterruptedException e) {
+			} catch (ExecutionException e) {
+			}
+
+		}
+
+		// Sort in ascending order
+		binFileSearcher.sort(resultIndexList);
+
+		_profile_lastEndTime = System.currentTimeMillis();
+
+		return resultIndexList;
+	}
+
+	private final class BigFileSearchTask implements Callable<List<Long>> {
+
+		final int workerSize;
+		final int workerNumber;
+		final File srcFile;
+		final byte[] searchBytes;
+
+		final long startPostion;
+		final long readLeng;
+
+		final BinFileProgressListenerEx binFileProgressListener;
+
+		BigFileSearchTask(File srcFile, byte[] searchBytes, long startPosition, long readLeng, int workerNumber, int workerSize, BinFileProgressListenerEx listener) {
+
+			this.srcFile = srcFile;
+			this.startPostion = startPosition;
+			this.readLeng = readLeng;
+			this.searchBytes = searchBytes;
+			this.binFileProgressListener = listener;
+			this.workerNumber = workerNumber;
+			this.workerSize = workerSize;
+		}
+
+		public List<Long> call() throws Exception {
+
+			final BinFileSearcher blockSearchWorker = new BinFileSearcher();
+			blockSearchWorker.setBufferSize(bufferSizePerWorker);
+			blockSearchWorker.setSubThreadSize(subThreadSize);
+			blockSearchWorker.setSubBufferSize(subBufferSize);
+
+			if (this.binFileProgressListener != null) {
+				blockSearchWorker.setBigFileProgressListener(new BinFileProgressListener() {
+
+					@Override
+					public void onProgress(List<Long> pointerList, float progress, float currentPosition, float startPosition, long maxSizeToRead) {
+						binFileProgressListener.onProgress(workerNumber, workerSize, pointerList, progress);
+					}
+				});
+			}
+
+			final List<Long> pointerList = blockSearchWorker.searchPartially(srcFile, searchBytes, startPostion, readLeng);
+
+			return pointerList;
+		}
+	}
+
+	final static class ProgressCache {
+
+		volatile float[] progress;
+
+		final int workerSize;
+
+		ProgressCache(int workerSize) {
+			this.workerSize = workerSize;
+			this.progress = new float[workerSize];
+		}
+
+		synchronized void setProgress(int workerNumber, float progress) {
+			this.progress[workerNumber] = progress;
+		}
+
+		float getProgress() {
+			float progress = 0;
+			for (int i = 0; i < workerSize; i++) {
+				progress += this.progress[i];
+			}
+			return progress / (float) workerSize;
+		}
+	}
+
+	// Call from each worker thread
+	private void onProgress(int workerNumber, int workerSize, List<Long> pointerList, float progress) {
+		if (progressCache == null) {
+			progressCache = new ProgressCache(workerSize);
+		}
+		progressCache.setProgress(workerNumber, progress);
+		bigFileProgressListener.onProgress(progressCache.getProgress());
+	}
+
+	/**
+	 * 
+	 * Get operation time in millis of last search
+	 * 
+	 * @return
+	 */
+	public long getEllapsedMillis() {
+		return _profile_lastEndTime - _profile_lastStartTime;
+	}
+
+	/**
+	 * Profiling method<br>
+	 */
+	public void _showProfile() {
+		System.out.println("availableProcessors=" + Runtime.getRuntime().availableProcessors() + " free memory=" + getMegaBytes(Runtime.getRuntime().freeMemory()));
+		System.out.println(
+					"worker blockSize=" + getMegaBytes(blockSize) + " " +
+							"worker buffer Size=" + getMegaBytes(bufferSizePerWorker) + ", " +
+							"max num of thread=" + maxNumOfThreads + ", " +
+							"sub buffer size=" + subBufferSize + "(B)" + ", " +
+							"sub thread size=" + subThreadSize + ", "
+					);
+
+		System.out.println("possible max thread=" + maxNumOfThreads * subThreadSize + " " +
+				"possible max memory=" + getMegaBytes(bufferSizePerWorker * maxNumOfThreads + (subBufferSize * subThreadSize))
+					);
+	}
+
+	/**
+	 * Optimize threading and memory
+	 * 
+	 * @param fileLength
+	 */
+	private void optimize(long fileLength) {
+
+		final int availableProcessors = Runtime.getRuntime().availableProcessors();
+
+		final long free = Runtime.getRuntime().freeMemory() / 2;
+
+		int workerSize = availableProcessors / 2;
+		if (workerSize < 2) {
+			workerSize = 2;
+		}
+
+		long bufferSize = free / workerSize;
+		if (bufferSize > 4 * 1024 * 1024) {
+			bufferSize = 4 * 1024 * 1024;
+		}
+		int iBufferSize = (int) bufferSize;
+
+		long blockSize = fileLength / workerSize;
+		if (blockSize > 16 * 1024 * 1024) {
+			blockSize = 16 * 1024 * 1024;
+		}
+		int iBlockSize = (int) blockSize;
+
+		this.setBlockSize(iBlockSize);
+		this.setMaxNumOfThreads(workerSize);
+		this.setBufferSizePerWorker(iBufferSize);
+		this.setSubBufferSize(256);
+
+	}
+
+	private String getMegaBytes(long valBytes) {
+		return String.format("%.1f(MB)", ((float) valBytes / (1024f * 1024f)));
+	}
+
+}
